cff83f5fa8cfe603e3ac9c49c95fa3245cfb1817
NunesNathan <nunesxd7@hotmail.com>
2022-10-01 13:32:50 -0300

    feat: episodio entity

--- src/main/java/com/trybe/acc/java/minhasseries/model/Episodio.java
+++ src/main/java/com/trybe/acc/java/minhasseries/model/Episodio.java
@@ -1,8 +1,46 @@
 package com.trybe.acc.java.minhasseries.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Episodio {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private int numero;
+  private int duracaoEmMinutos;
+  @ManyToOne
+  private Serie serie;
 
+  public Episodio() {
+  }
+
+  public Episodio(int numero, int duracaoEmMinutos) {
+    this.numero = numero;
+    this.duracaoEmMinutos = duracaoEmMinutos;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getDuracaoEmMinutos() {
+    return duracaoEmMinutos;
+  }
+
+  public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+    this.duracaoEmMinutos = duracaoEmMinutos;
+  }
 }
