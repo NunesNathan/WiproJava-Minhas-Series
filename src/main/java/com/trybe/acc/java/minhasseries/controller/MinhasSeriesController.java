c78d72e74297500cb3309c3a4e51363ddd56e4a7
NunesNathan <nunesxd7@hotmail.com>
2022-10-01 17:16:22 -0300

    feat: post serie route works

--- src/main/java/com/trybe/acc/java/minhasseries/controller/MinhasSeriesController.java
+++ src/main/java/com/trybe/acc/java/minhasseries/controller/MinhasSeriesController.java
@@ -5,6 +5,8 @@ import com.trybe.acc.java.minhasseries.service.MinhasSeriesService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,12 @@ public class MinhasSeriesController {
   private MinhasSeriesService minhasSeriesService;
 
   @GetMapping
-  public List<Serie> findAll() {
-    return minhasSeriesService.list();
+  public List<Serie> listar() {
+    return minhasSeriesService.listar();
+  }
+
+  @PostMapping
+  public Serie salvarUmaSerie(@RequestBody Serie serie) {
+    return minhasSeriesService.salvarUmaSerie(serie);
   }
 }
--- src/main/java/com/trybe/acc/java/minhasseries/service/MinhasSeriesService.java
+++ src/main/java/com/trybe/acc/java/minhasseries/service/MinhasSeriesService.java
@@ -1,5 +1,6 @@
 package com.trybe.acc.java.minhasseries.service;
 
+import com.trybe.acc.java.minhasseries.exception.SerieExistenteException;
 import com.trybe.acc.java.minhasseries.model.Serie;
 import com.trybe.acc.java.minhasseries.repository.SerieRepository;
 import java.util.List;
@@ -11,7 +12,16 @@ public class MinhasSeriesService {
   @Autowired
   private SerieRepository serieRepository;
 
-  public List<Serie> list() {
+  public List<Serie> listar() {
     return serieRepository.findAll();
+  }
+
+  /** save serie method.*/
+  public Serie salvarUmaSerie(Serie serie) {
+    if (serieRepository.existsByNome(serie.getNome())) {
+      throw new SerieExistenteException();
+    }
+
+    return serieRepository.save(serie);
   }
 }
