370a4a33eee9e295459845bc8330aea631a0130c
NunesNathan <nunesxd7@hotmail.com>
2022-10-01 13:39:51 -0300

    feat: serie entity

--- src/main/java/com/trybe/acc/java/minhasseries/model/Serie.java
+++ src/main/java/com/trybe/acc/java/minhasseries/model/Serie.java
@@ -1,9 +1,47 @@
 package com.trybe.acc.java.minhasseries.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Serie {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String nome;
+  @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+  private List<Episodio> episodios = new ArrayList<>();
 
+  public Serie() {
+  }
 
+  public Serie(String nome) {
+    this.nome = nome;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Episodio> getEpisodios() {
+    return episodios;
+  }
+
+  public void adicionarEpisodio(Episodio episodios) {
+    this.episodios.add(episodios);
+  }
 }
